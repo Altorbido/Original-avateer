1b23e3a27024ad5b2663e9eb6d1935979012d24d
Altorbido <myhind0@gmail.com>
2021-09-17 11:49:54 -1000

    NEW STUFF

---
+++ Assets/btnFX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class btnFX : MonoBehaviour
+{
+	public AudioSource myFx;
+	public AudioClip hoverFx;
+	public AudioClip clickFx;
+	public AudioClip BackSound;
+	public void HoverSound()
+	{
+		myFx.PlayOneShot (hoverFx);
+	}
+	public void ClickSound()
+	{
+		myFx.PlayOneShot (clickFx);
+	}
+	public void backSound (){
+		myFx.PlayOneShot (BackSound);	
+	}
+}
